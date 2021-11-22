963a3ec062faa7222bb71a8fab73ee45fbe1c6a1
Skecsup <kicsindy.julis@gmail.com>
2021-11-22 22:05:11 +0100

    kezdetleges navbar, tobb styling szukseges

---
+++ src/components/navbar/Navbar_Style.js
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  background-color: #eae4f6;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  @media (max-width: 768px) {
+    border-bottom-left-radius: 20px;
+    border-bottom-right-radius: 20px;
+  }
+`;
+export const Logo = styled.div`
+  margin: 5px 5px;
+  padding: 5px;
+  cursor: pointer;
+  img {
+    height: 80px;
+  }
+`;
+
+export const Menu = styled.div`
+  margin: 0 10px;
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    margin: ${(props) => (props.isOpen ? "10px 10px" : "0")};
+    overflow: hidden;
+    flex-direction: column;
+    width: 100%;
+    max-height: ${(props) => (props.isOpen ? "300px" : "0")};
+    transition: margin 0.1s ease-in-out, max-height 0.4s ease-in-out;
+  }
+`;
+export const MenuLink = styled.div`
+  padding: 10px 10px;
+  color: #997cd6;
+  cursor: pointer;
+  font-size: 25px;
+  font-weight: 100;
+`;
+export const Hamburger = styled.div`
+  display: none;
+  flex-direction: column;
+  padding: 10px;
+  cursor: pointer;
+
+  span {
+    height: 4px;
+    width: 25px;
+    margin-bottom: 4px;
+    border-radius: 2px;
+    background-color: #997cd6;
+  }
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+export const Signin = styled.div`
+  margin-left: 30px;
+  padding: 10px 20px;
+  background-color: #997cd6;
+  border-radius: 25px;
+  color: white;
+  font-weight: 100;
+  cursor: pointer;
+  font-size: 25px;
+`;
