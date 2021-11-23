09f4805db34cedae2fcd9bfde7a4d0d627d6c96a
Skecsup <kicsindy.julis@gmail.com>
2021-11-23 12:02:05 +0100

    routing beallitva, text decoration leszedve a linkrol, nem a legszebb te megteszi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import Navbar from "./components/navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Shop from "./components/pages/Shop";
+import About from "./components/pages/About";
+import Basket from "./components/pages/Basket";
+import Signin from "./components/pages/Signin";
 
 function App() {
+  const home = [<Navbar />, <Home />];
+  const shop = [<Navbar />, <Shop />];
+  const about = [<Navbar />, <About />];
+  const basket = [<Navbar />, <Basket />];
+  const signin = [<Navbar />, <Signin />];
+
   return (
     <div className="App">
-      <Navbar />
+      <Routes>
+        <Route path="/" element={home} />
+        <Route path="/Shop" element={shop} />
+        <Route path="/About" element={about} />
+        <Route path="/Basket" element={basket} />
+        <Route path="/Signin" element={signin} />
+      </Routes>
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -9,25 +9,43 @@ import {
 } from "./Navbar_Style";
 
 import logo from "../../assets/img/vizjel.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+
+  const style = {
+    textDecoration: "none",
+  };
+
   return (
     <Container>
-      <Logo>
-        <img src={logo} alt="logo" />
-      </Logo>
+      <Link to="/">
+        <Logo>
+          <img src={logo} alt="logo" />
+        </Logo>
+      </Link>
       <Hamburger onClick={() => setIsOpen(!isOpen)}>
         <span />
         <span />
         <span />
       </Hamburger>
       <Menu isOpen={isOpen}>
-        <MenuLink>HOME</MenuLink>
-        <MenuLink>SHOP</MenuLink>
-        <MenuLink>ABOUT US</MenuLink>
-        <MenuLink>BASKET</MenuLink>
-        <Signin>SIGN IN</Signin>
+        <Link style={style} to="/">
+          <MenuLink>HOME</MenuLink>
+        </Link>
+        <Link style={style} to="/Shop">
+          <MenuLink>SHOP</MenuLink>
+        </Link>
+        <Link style={style} to="/About">
+          <MenuLink>ABOUT US</MenuLink>
+        </Link>
+        <Link style={style} to="/Basket">
+          <MenuLink>BASKET</MenuLink>
+        </Link>
+        <Link style={style} to="/Signin">
+          <Signin>SIGN IN</Signin>
+        </Link>
       </Menu>
     </Container>
   );
